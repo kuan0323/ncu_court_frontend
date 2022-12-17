cd7fede4c579a98f5a5efc3a325dd7f1aff5b8da
FuHarrison <fuh.harrison.lex@gmail.com>
2022-12-18 03:43:35 +0800

    add component-NavBar

--- pages/basketball_courts.tsx
+++ pages/basketball_courts.tsx
@@ -1,4 +1,6 @@
-const products = [
+import NavBar from "../components/NavBar";
+
+const courts = [
   {
     id: 1,
     name: "籃球場A",
@@ -44,41 +46,44 @@ const products = [
     price: "$999",
     beReserved: "已被預約",
   },
-  // More products...
+  // More courts...
 ];
 
 export default function basketball_courts() {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="mb-5 text-2xl font-bold tracking-tight text-teal-500">
+      <div>
+        <NavBar mainPage={false} myReserve={false} myAccount={false}></NavBar>
+      </div>
+      <div className=" mx-auto max-w-2xl py-8 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="mb-5 text-2xl font-bold tracking-tight text-theme">
           籃球場列表
         </h2>
-        <div className="bg-gray-500 h-0.5"></div>
+        <div className="bg-gray-400 h-0.5"></div>
         <div className="mt-16 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-          {products.map((product) => (
-            <div key={product.id} className="group relative">
+          {courts.map((court) => (
+            <div key={court.id} className="group relative">
               <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                 <img
-                  src={product.photo}
-                  alt={product.photoAlt}
+                  src={court.photo}
+                  alt={court.photoAlt}
                   className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
               </div>
               <div className="mt-4 flex justify-between">
                 <div>
                   <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
+                    <a href={court.href}>
                       <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
+                      {court.name}
                     </a>
                   </h3>
                   <p className="mt-1 text-sm text-gray-500">
-                    {product.beReserved}
+                    {court.beReserved}
                   </p>
                 </div>
                 <p className="text-sm font-medium text-gray-900">
-                  {product.price}
+                  {court.price}
                 </p>
               </div>
             </div>
