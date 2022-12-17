e1373d631650a8085b61317c88b55bc5f8c81f8d
chieh-eric <ericwang0621@gmail.com>
2022-12-17 08:11:40 +0800

    pages_reserve_record

--- pages/reserve_record.tsx
+++ pages/reserve_record.tsx
@@ -3,17 +3,16 @@ import Button from '../components/Button'
 export default function admin_member() {
     return (
         <div>
-            <div className=" col-span-2 text-center bg-emerald 300">
+            <div className=" text-xl text-center bg-teal-500">
                 <Button value={'我的預約'} ></Button>
                 <Button value={'中央大學場地預約網'}></Button>
                 <Button value={'我的帳戶'}></Button>
-                <button type="button" className="text-right hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 ">
-                    登出
-                </button>
+                <Button value={'登出'}></Button>
+
             </div>
-           
+
             <section>
-                <div className="flex flex-col ml-60">
+                <div className="flex flex-col">
                     <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                             <div className="overflow-hidden">
@@ -21,20 +20,27 @@ export default function admin_member() {
                                     <thead className="border-b">
                                         <tr>
                                             <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                姓名
+                                                圖片
                                             </th>
                                             <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                學號
+                                                場地類別
                                             </th>
                                             <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                Email
+                                                場地編號
                                             </th>
                                             <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                電話
+                                                預約時間
                                             </th>
                                             <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                                                 操作
                                             </th>
+                                        </tr>
+                                        <tr>
+                                            <th></th>
+                                            <th></th>
+                                            <th></th>
+                                            <th></th>
+                                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left"><Button value={'取消預約'}></Button></th>
                                         </tr>
                                     </thead>
 
