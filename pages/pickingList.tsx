b3f7a631a2f0036059b261b183c6b215811fe3c4
chieh-eric <ericwang0621@gmail.com>
2022-12-21 15:17:40 +0800

    add page pickingList content

--- pages/pickingList.tsx
+++ pages/pickingList.tsx
@@ -1,55 +1,132 @@
+import NavBar from "../components/NavBar";
 import styles from '../styles/Home.module.css'
 import Button from '../components/Button'
-export default function pickingList() {
+
+const records = [
+    {
+        id: "1",
+        href: "#",
+        photo: "",
+        photoAlt: "圖片",
+        category: "籃球",
+        courtNum: "3",
+        courtStatus: "尚無空場"
+    },
+    {
+        id: "2",
+        href: "#",
+        photo: "",
+        photoAlt: "圖片",
+        category: "羽球",
+        courtNum: "4",
+        courtStatus: "尚無空場"
+    },
+    {
+        id: "3",
+        href: "#",
+        photo: "",
+        photoAlt: "圖片",
+        category: "排球",
+        courtNum: "1",
+        courtStatus: "尚無空場"
+    },
+    {
+        id: "4",
+        href: "#",
+        photo: "",
+        photoAlt: "圖片",
+        category: "桌球",
+        courtNum: "2",
+        courtStatus: "尚無空場"
+    },
+    {
+        id: "5",
+        href: "#",
+        photo: "",
+        photoAlt: "圖片",
+        category: "籃球",
+        courtNum: "1",
+        courtStatus: "尚無空場"
+    },
+];
+export default function reserveRecord() {
     return (
         <div>
-            <div className=" text-xl text-center bg-teal-500">
-                <Button value={'我的預約'} ></Button>
-                <Button value={'中央大學場地預約網'}></Button>
-                <Button value={'我的帳戶'}></Button>
-                <Button value={'登出'}></Button>
-
+            <div>
+                <NavBar mainPage={false} myReserve={false} myAccount={false}></NavBar>
             </div>
+            <div className=" mx-auto max-w-2xl py-8 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
+                <h2 className="mb-5 text-2xl font-bold tracking-tight text-theme">
+                    待選清單
+                </h2>
+                <p className="mt-1 text-sm text-gray-500">
+                    加入待選清單的場地，後續如有釋出將會寄送E-mail另行通知！
+                </p>
+                <div className="bg-gray-400 h-0.5"></div>
 
-            <section>
-                <div className="flex flex-col">
-                    <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                            <div className="overflow-hidden">
-                                <table className="min-w-full">
-                                    <thead className="border-b">
-                                        <tr>
-                                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                圖片
-                                            </th>
-                                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                場地類別
-                                            </th>
-                                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                場地編號
-                                            </th>
-                                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                預約時間
-                                            </th>
-                                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                操作
-                                            </th>
-                                        </tr>
-                                        <tr>
-                                            <th></th>
-                                            <th></th>
-                                            <th></th>
-                                            <th></th>
-                                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left"><Button value={'取消預約'}></Button></th>
-                                        </tr>
-                                    </thead>
+                <div className="mt-3 grid grid-cols-1 gap-y-5 gap-x-6 sm:grid-cols-2 lg:grid-cols-1 xl:gap-x-8">
+                    <div className="flex flex-col">
+                        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                                <div className="overflow-hidden">
+                                    <table className="min-w-full">
+                                        <thead className="border-b">
+                                            <tr className="text-xl">
+                                                <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">
+                                                    圖片
+                                                </th>
+                                                <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">
+                                                    場地類別
+                                                </th>
+                                                <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">
+                                                    場地編號
+                                                </th>
+                                                <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">
+                                                    預約時間
+                                                </th>
+                                                <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">
+                                                    操作
+                                                </th>
+                                            </tr>
+                                            {records.map((record) => (
+                                                <tr>
+                                                    <td>
+                                                        <img
+                                                            src={record.photo}
+                                                            alt={record.photoAlt}
+                                                            className="h-full w-full object-cover object-center lg:h-full lg:w-full text-center"
+                                                        />
+                                                    </td>
+                                                    <td>
+                                                        <p className="text-sm font-medium text-gray-900 text-center">
+                                                            {record.category}
+                                                        </p>
+                                                    </td>
+                                                    <td>
+                                                        <p className="text-sm font-medium text-gray-900 text-center">
+                                                            {record.courtNum}
+                                                        </p>
+                                                    </td>
+                                                    <td>
+                                                        <p className="text-sm font-medium text-gray-900 text-center">
+                                                            {record.courtStatus}
+                                                        </p>
+                                                    </td>
+                                                    <td scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center"><Button value={'取消待選'}></Button></td>
+                                                </tr>
+                                            ))}</thead>
 
-                                </table>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
+
+
+
                 </div>
-            </section>
+            </div>
         </div>
     )
+
 }
