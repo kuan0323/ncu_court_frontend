3276ee3e116496c70621bb352b775914ceede0ae
kuan0323 <ronni31027@gmail.com>
2023-01-03 11:49:36 +0800

    fix: add key

--- pages/editAccount.tsx
+++ pages/editAccount.tsx
@@ -78,11 +78,11 @@ export default function EditAccount() {
 
                                         <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">修改會員資料</h3>
 
-                                        <form className="space-y-6" action="#">
+                                        <form className="space-y-6" action="#" >
                                             {
                                                 users.map(
                                                     (a: any) =>
-                                                        <div>
+                                                        <div key={a.studentId}>
                                                             <div className="flex gap-5">
 
                                                                 <div className="w-full">
@@ -142,9 +142,9 @@ export default function EditAccount() {
 
                                             <div className="flex gap-5">
 
-                                                <a href="/" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                <link href="/" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                     取消
-                                                </a>
+                                                </link>
                                                 <button
                                                     className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                                     onClick={() => editHandler()}
