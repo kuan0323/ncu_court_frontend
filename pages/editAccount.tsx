b2b53565d8c7715f70af5a58f4d2e172b5584e8a
kuan0323 <ronni31027@gmail.com>
2023-01-04 23:45:24 +0800

    fix: add key & change link tag

--- pages/editAccount.tsx
+++ pages/editAccount.tsx
@@ -82,7 +82,7 @@ export default function EditAccount() {
                                             {
                                                 users.map(
                                                     (a: any) =>
-                                                        <div>
+                                                        <div key={a.name}>
                                                             <div className="flex gap-5">
 
                                                                 <div className="w-full">
@@ -142,9 +142,9 @@ export default function EditAccount() {
 
                                             <div className="flex gap-5">
 
-                                                <a href="/" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                <link href="/" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                     取消
-                                                </a>
+                                                </link>
                                                 <button
                                                     className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                                     onClick={() => editHandler()}
